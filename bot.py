bbcb8f5467c5a562910d336dd72201aebd7b1376
Nima <nima10khodaveisi@gmail.com>
2020-06-18 14:03:25 +0430

    first commit

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ API_TOKEN = '1015161255:AAEnNiql2HzZm3DNMU1ibUyHaK4gj8jYjlQ'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler()
 def tag_spei(message) :
     bot.reply_to(message, '@IsepantaI')
+    print(message)
 
 bot.polling()
-#hey
