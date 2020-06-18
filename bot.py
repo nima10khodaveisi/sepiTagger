e5bf87c1e211ee47b64466a58c5ad8cf9ae43b3e
Nima <nima10khodaveisi@gmail.com>
2020-06-18 20:09:45 +0430

    second comimit

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
+from asyncore import dispatcher
+
 import telebot
 
-API_TOKEN = '1015161255:AAEnNiql2HzZm3DNMU1ibUyHaK4gj8jYjlQ'
+API_TOKEN = '1104495525:AAGshNjPqxeMmZ7IAOy675YSnPrHEOJ6TMQ'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler()
 def tag_spei(message) :
-    bot.reply_to(message, '@IsepantaI')
+    bot.send_animation(message.chat.id, open('IMG_2923.MP4','rb'), reply_to_message_id=message.message_id)
     print(message)
+
 
 bot.polling()
